96f4cb12d1eccb41b2a5341ce606b752c05d5962
Kevin5051 <kevin_515@163.com>
2015-11-26 14:56:51 +0800

    test

---
+++ src/com/common/bean/Bean.java
@@ -0,0 +1,18 @@
+package com.common.bean;
+
+/**  
+ *   
+ * 项目名称：SpringMVC  
+ * 版本：V1.5.2
+ * 类名称：Bean  
+ * 类描述：  
+ * 创建人：Kevin
+ * 创建时间：2015年11月26日 下午2:46:49  
+ * 修改人：Administrator  
+ * 修改时间：2015年11月26日 下午2:46:49  
+ * 修改备注：   
+ *   
+ */
+public class Bean {
+
+}
